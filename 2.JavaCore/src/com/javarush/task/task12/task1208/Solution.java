1dd485e3f3058d14c866dbc487cd3767a1e21c62
IlyaLukovnikov <il.dm.luk@mail.ru>
2018-07-31 15:30:34 +0300

    Solved 1206 - 1210

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    static void print() {
+
+    }
+
+    static void print(int x) {
+
+    }
+
+    static void print(String s) {
+
+    }
+
+    static void print(long l) {
+
+    }
+
+    static void print(double d) {
+
+    }
+}
