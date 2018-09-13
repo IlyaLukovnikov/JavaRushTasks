7a321f656f298bed8958423ed08e981b6c282fb1
IlyaLukovnikov <il.dm.luk@mail.ru>
2018-09-13 18:44:44 +0300

    Added 1523 - 1524

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public Solution() {
+    }
+
+    private Solution(int i) {
+
+    }
+
+    protected Solution(double d) {
+
+    }
+
+    Solution(String s) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
